ca4182395aa591869782e2268b9ab4cfc195068c
Zatrackz <pinky.star17@yahoo.com>
2023-02-25 18:02:00 -0500

    New script

---
+++ Ponnia_test/hello.sh
@@ -0,0 +1,23 @@
+# Author: Ponnia Muyen
+# Description: This script has a couple of read statements that request information from the user
+# Date: 02-25-2023
+
+
+echo "What is your name? "
+read name
+
+echo "How old are you? "
+read age
+
+echo "Where are you from? "
+read origin
+
+
+echo "How can we help you? "
+read reason
+
+echo "Hello ${name}, you are ${age} years old, you are originally from ${origin}, and the reason you are coming to our store is: ${reason} "
+
+
+
+
